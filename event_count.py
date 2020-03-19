2181173c1a73f55e7631bf3ab0c814e161981aa4
ndelasoul <48021240+ndelasoul@users.noreply.github.com>
2020-03-19 22:46:31 +0800

    Update event_count.py

--- event_count.py
+++ event_count.py
@@ -10,45 +10,35 @@ import os
 import mne
 import pprint
 
-def load(in_vhdr):
-    raw = mne.io.read_raw_brainvision(in_vhdr, preload=True)
-    return raw
-
-def annotate(in_vmrk, raw):
-    annot = mne.read_annotations(in_vmrk)
-    annot.save('annotations.txt')
-    return annot, raw
-
-
-def combine(annot, raw):
+def combine(annot):
     raw.set_annotations(annot)
-    return annot, raw
+    reconstruct(raw)
+    return 
 
 def reconstruct(raw):
     events, event_ids = mne.events_from_annotations(raw)
-    print(event_ids)
+    results(events)
     return
     
-"""
-def results(events, in_num):
-    e_stim = mne.pick_events(events, include=in_num)
+
+def results(events):
+    in_num = input("Which event: ")
+    e_stim = mne.pick_events(events, include=int(in_num))
     print(e_stim.shape)
     return
-"""
+
+def load(in_vhdr, in_vmrk):
+    raw = mne.io.read_raw_brainvision(in_vhdr, preload=True)
+    annot = mne.read_annotations(in_vmrk)
+    combine(annot)
+    return 
+
 
 if __name__ == '__main__':
     cwd_files = glob(os.path.join(os.getcwd(),'*'))
     pprint.pprint(cwd_files)
     in_vhdr = input("vhdr file: ")
     fdir = os.path.join(os.getcwd(),in_vhdr)
-    load(fdir)
-
     in_vmrk = input("vmrk file: ")
     edir = os.path.join(os.getcwd(),in_vmrk)
-    annotate(edir)
-"""
-    in_num = input("Which event:")
-    ndir = os.path.join(os.getcwd(),in_num)
-    results(in_num)
-"""
-    
+    load(in_vhdr, in_vmrk)
